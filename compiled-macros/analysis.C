74384aa7f4fb36de54827f19eb4d6fbb4d038bca
David Bjergaard <dbjergaard@gmail.com>
2014-03-30 20:13:16 -0400

    restructured slightly for web publishing

--- compiled-macros/analysis.C
+++ compiled-macros/analysis.C
@@ -1,7 +1,12 @@
+#include <cstdlib>
 #include "TFile.h"
 #include "TH1F.h"
 
 
+// Generates a random number between min and max
+double randomP(double min, double max){
+  return (max-min)*((double)rand())/RAND_MAX+min;
+}
 
 int doAnalysis(){
   //Since the ROOT binary already defines a "main" an error will occur
@@ -16,9 +21,17 @@ int doAnalysis(){
   //variables to hold our data
   double jet_E=0,jet_px=0,jet_py=0,jet_pz=0;
 
+  //initialize random seed, use a fixed number to make the results
+  //repeatable 
+  srand(42);
+
   //this is the eventloop, normally this is where you analyze tree
   //branches, but since there's no input data 
   for(size_t i=0; i < N; i++  ){
-    
+    jet_E=randomP(0,100);
+    jet_px=randomP(0,100);
+    jet_py=randomP(0,100);
+    jet_pz=randomP(0,100);
   }
+  return 42;
 }
