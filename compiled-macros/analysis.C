2f31e2fdee84aa96750edbbc076453c9ea2466f7
David Bjergaard <dbjergaard@gmail.com>
2014-05-16 18:00:03 -0400

    latest info

--- compiled-macros/analysis.C
+++ compiled-macros/analysis.C
@@ -15,18 +15,14 @@ int doAnalysis(){
   
   //Open files and retrieve pointers here
 
-  //number of events to process, usually if you are processing a tree, this is tree->GetEntries();
   const int N=10000;
 
-  //variables to hold our data
   double jet_E=0,jet_px=0,jet_py=0,jet_pz=0;
 
   //initialize random seed, use a fixed number to make the results
   //repeatable 
   srand(42);
 
-  //this is the eventloop, normally this is where you analyze tree
-  //branches, but since there's no input data 
   for(size_t i=0; i < N; i++  ){
     jet_E=randomP(0,100);
     jet_px=randomP(0,100);
--- compiled-program/src/analysis.C
+++ compiled-program/src/analysis.C
@@ -11,24 +11,21 @@ double randomP(double min, double max){
 }
 
 int doAnalysis(){
-  //Since the ROOT binary already defines a "main" an error will occur
-  //if you redefine another function named "main", therefore we use
-  //the verb "doAnalysis"
-  
-  //Open files and retrieve pointers here
+  //This is the analysis driver, we initialize a number of events to
+  //process, create variables to hold relevant quantities, and execute
+  //the event loop.  Typically there will be selection criteria
+  //applied at this stage.  Histograms and other output are also
+  //usually produced here.
 
-  //number of events to process, usually if you are processing a tree, this is tree->GetEntries();
+
   const int N=100;
-
-  //variables to hold our data
   double jet_E=0.,jet_px=0.,jet_py=0.,jet_pz=0.;
 
   //initialize random seed, use a fixed number to make the results
-  //repeatable 
+  //repeatable, be especially careful that you choose unique seeds if
+  //you are generating random numbers in batch systems!
   srand(42);
 
-  //this is the eventloop, normally this is where you analyze tree
-  //branches, but since there's no input data 
   TLorentzVector jet;
   for(size_t i=0; i < N; i++  ){
     jet_E=randomP(0,100);
